42ad7ec7d9cb81d21d648a61f65d0c401da87f8b
Chavez Jr <Juan.ChavezJr@med.usc.edu>
2016-09-20 14:37:12 -0700

    debug

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -44,7 +44,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-ms-adal": "0.8.0"
+    "cordova-plugin-ms-adal": "0.8.0",
+    "cordova-plugin-whitelist": "1.2.2"
 };
 // BOTTOM OF METADATA
 });
